cd5fcbdf4714173e06ff4ef087a2f59cf55ab0d4
kings1ay3r <carolchriskevin@gmail.com>
2021-11-02 14:36:18 +0530

    Concurrency v1

---
+++ concurrency/v1/concurrency.go
@@ -0,0 +1,22 @@
+package main
+
+type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+	rChan := make(chan result)
+	for _, url := range urls {
+		go func(u string) {
+			rChan <- result{u, wc(u)}
+		}(url)
+	}
+	for i := 0; i < len(urls); i++ {
+		r := <-rChan
+		results[r.string] = r.bool
+	}
+	return results
+}
