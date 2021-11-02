e4512b769eefdfd01debcae646105a768889111d
kings1ay3r <carolchriskevin@gmail.com>
2021-11-02 15:21:02 +0530

    Select v2

---
+++ select/v2/select.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"time"
+)
+
+func main() {
+
+}
+
+func Racer(a, b string) (winner string) {
+	aDuration := measureResponseTime(a)
+	bDuration := measureResponseTime(b)
+
+	if aDuration < bDuration {
+		return a
+	}
+	return b
+}
+func measureResponseTime(a string) time.Duration {
+
+	startA := time.Now()
+	http.Get(a)
+	return time.Since(startA)
+}
