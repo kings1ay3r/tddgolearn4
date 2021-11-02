bc41332886fd0f4298a34138f4fb42010a2a0541
kings1ay3r <carolchriskevin@gmail.com>
2021-11-02 13:09:59 +0530

    Mocking v4

--- mocking/v3/mock.go
+++ mocking/v3/mock.go
@@ -2,8 +2,18 @@ package main
 
 import "time"
 
+const write = "write"
+
+const sleep = "sleep"
+
 type Sleeper interface {
 	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (*DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
 }
 
 type SpySleeper struct {
@@ -14,8 +24,15 @@ func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
 
-type DefaultSleeper struct{}
+type SpyCountdownOperations struct {
+	Calls []string
+}
 
-func (*DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
 }
