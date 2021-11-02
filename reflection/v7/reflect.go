cc8b540834de94e5acec697d1b9c7f2ad55ed2e4
kings1ay3r <carolchriskevin@gmail.com>
2021-11-02 18:53:19 +0530

    Reflection v7

---
+++ reflection/v7/reflect.go
@@ -0,0 +1,41 @@
+package main
+
+import "reflect"
+
+func walk(x interface{}, fn func(input string)) {
+	val, kind := getValue(x)
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
+	}
+	/* if field.Kind() == reflect.String {
+		fn(field.String())
+	} */
+
+	switch kind {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			field := val.Field(i)
+			walkValue(field)
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			field := val.Index(i)
+			walkValue(field)
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walkValue(val.MapIndex(key))
+		}
+	}
+}
+func getValue(x interface{}) (val reflect.Value, kind reflect.Kind) {
+
+	val = reflect.ValueOf(x)
+	kind = val.Kind()
+	if kind == reflect.Ptr {
+		val = val.Elem()
+	}
+	return
+}
---
+++ reflection/v7/reflect_test.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+	expected := "Chris"
+	var got []string
+
+	x := struct {
+		Name string
+	}{expected}
+
+	walk(x, func(input string) {
+		got = append(got, input)
+	})
+	if len(got) != 1 {
+		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+	}
+
+	if got[0] != expected {
+		t.Errorf("Incorrect Value Recieved")
+	}
+
+}
+
+func TestWalk2(t *testing.T) {
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Nested fields",
+			struct {
+				Name    string
+				Profile struct {
+					Age     int
+					City    string
+					Address struct {
+						zip string
+						pin int
+					}
+				}
+			}{
+				"Kevin",
+				struct {
+					Age     int
+					City    string
+					Address struct {
+						zip string
+						pin int
+					}
+				}{
+					33,
+					"Wayanad",
+					struct {
+						zip string
+						pin int
+					}{"xyz", 123},
+				},
+			},
+			[]string{"Kevin", "Wayanad", "xyz"},
+		},
+		{
+			"Pointers to things",
+			&struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Slices",
+			[]struct {
+				Age  int
+				City string
+			}{
+				{33, "London"},
+				{34, "Tokyo"},
+			},
+			[]string{"London", "Tokyo"},
+		},
+		{
+			"Arrays",
+			[2]string{"Japan", "Mauritius"},
+			[]string{"Japan", "Mauritius"},
+		},
+		{
+			"Maps",
+			map[string]string{"Country": "Japan", "Capital": "Mauritius"},
+			[]string{"Japan", "Mauritius"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+			if test.Name == "Maps" {
+				assertMapsEqual(t, got, test.ExpectedCalls)
+			} else {
+
+				if !reflect.DeepEqual(got, test.ExpectedCalls) {
+					t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+				}
+			}
+		})
+	}
+}
+func assertMapsEqual(t *testing.T, got, want []string) {
+
+	t.Helper()
+	if len(got) != len(want) {
+		t.Errorf("Different Length Slices")
+		return
+	}
+	for _, x := range want {
+		contains := false
+		for _, y := range got {
+			if y == x {
+				contains = true
+			}
+			if !contains {
+				t.Errorf("Expected %v to contain %v", y, want)
+			}
+		}
+	}
+
+}
