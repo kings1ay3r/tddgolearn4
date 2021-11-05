2887bce0af5c31433d8d5cd3f5a7564c7fe7fb06
kings1ay3r <carolchriskevin@gmail.com>
2021-11-05 13:48:48 +0530

    IO v2

---
+++ io/v2/file_system_player_store.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+)
+
+type FileSystemPlayerStore struct {
+	database io.ReadWriteSeeker
+}
+
+func (f *FileSystemPlayerStore) GetLeague() []Player {
+	f.database.Seek(0, 0)
+	league, _ := NewLeague(f.database)
+	return league
+}
+func (f *FileSystemPlayerStore) GetPlayerScore(name string) (wins int) {
+	for _, player := range f.GetLeague() {
+		if player.Name == name {
+			wins = player.Wins
+			break
+		}
+	}
+
+	return
+}
+func (f *FileSystemPlayerStore) RecordWin(name string) {
+	league := f.GetLeague()
+	for i, player := range league {
+		if player.Name == name {
+			league[i].Wins++
+		}
+	}
+	f.database.Seek(0, 0)
+	json.NewEncoder(f.database).Encode(league)
+}
