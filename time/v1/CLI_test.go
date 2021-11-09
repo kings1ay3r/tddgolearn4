906708d6a10d3443d60bfb91c8d28b3d1e5bf8b8
kings1ay3r <carolchriskevin@gmail.com>
2021-11-09 11:37:28 +0530

    Time V1

---
+++ time/v1/CLI_test.go
@@ -0,0 +1,131 @@
+package poker_test
+
+import (
+	"fmt"
+	"io"
+	"strings"
+	poker "tddgolearn4/time/v1"
+	"testing"
+	"time"
+)
+
+func TestCLI(t *testing.T) {
+	dummySpyAlerter := &spyBlindAlerter{}
+	t.Run("record chris win from user input", func(t *testing.T) {
+		in := strings.NewReader("Chris wins\n")
+		playerStore := &poker.StubPlayerStore{}
+
+		cli := poker.NewCLI(playerStore, in, dummySpyAlerter)
+		cli.PlayPoker()
+
+		poker.AssertPlayerWin(t, playerStore, "Chris")
+	})
+
+	t.Run("record cleo win from user input", func(t *testing.T) {
+		in := strings.NewReader("Cleo wins\n")
+		playerStore := &poker.StubPlayerStore{}
+
+		cli := poker.NewCLI(playerStore, in, dummySpyAlerter)
+		cli.PlayPoker()
+
+		poker.AssertPlayerWin(t, playerStore, "Cleo")
+	})
+
+	t.Run("do not read beyond the first newline", func(t *testing.T) {
+		in := failOnEndReader{
+			t,
+			strings.NewReader("Chris wins\n hello there"),
+		}
+
+		playerStore := &poker.StubPlayerStore{}
+
+		cli := poker.NewCLI(playerStore, in, dummySpyAlerter)
+		cli.PlayPoker()
+	})
+}
+
+func TestBlindAlerts(t *testing.T) {
+	t.Run("it schedules printing of blind values", func(t *testing.T) {
+		in := strings.NewReader("Chris wins\n")
+		playerStore := &poker.StubPlayerStore{}
+		blindAlerter := &spyBlindAlerter{}
+
+		cli := poker.NewCLI(playerStore, in, blindAlerter)
+		cli.PlayPoker()
+
+		if len(blindAlerter.alerts) < 1 {
+			t.Fatal("expected a blind alert to be scheduled")
+		}
+	})
+	t.Run("it schedules printing of multiple blind values", func(t *testing.T) {
+		in := strings.NewReader("Chris wins\n")
+		playerStore := &poker.StubPlayerStore{}
+		blindAlerter := &spyBlindAlerter{}
+
+		cli := poker.NewCLI(playerStore, in, blindAlerter)
+		cli.PlayPoker()
+
+		cases := []struct {
+			expectedScheduleTime time.Duration
+			expectedAmount       int
+		}{
+			{0 * time.Second, 100},
+			{10 * time.Minute, 200},
+			{20 * time.Minute, 300},
+			{30 * time.Minute, 400},
+			{40 * time.Minute, 500},
+			{50 * time.Minute, 600},
+			{60 * time.Minute, 800},
+			{70 * time.Minute, 1000},
+			{80 * time.Minute, 2000},
+			{90 * time.Minute, 4000},
+			{100 * time.Minute, 8000},
+		}
+
+		for i, c := range cases {
+			t.Run(fmt.Sprintf("%d scheduled for %v", c.expectedAmount, c.expectedScheduleTime), func(t *testing.T) {
+				if len(blindAlerter.alerts) <= i {
+					t.Fatalf("alert %d was not scheduled %v", i, blindAlerter.alerts)
+				}
+				alert := blindAlerter.alerts[i]
+				amountGot := alert.amount
+				if amountGot != c.expectedAmount {
+					t.Errorf("got amount %d, want %d", amountGot, c.expectedAmount)
+				}
+				gotScheduledTime := alert.scheduledAt
+				if gotScheduledTime != c.expectedScheduleTime {
+					t.Errorf("got scheduled time of %v, want %v", gotScheduledTime, c.expectedScheduleTime)
+				}
+			})
+		}
+	})
+}
+
+type failOnEndReader struct {
+	t   *testing.T
+	rdr io.Reader
+}
+
+func (m failOnEndReader) Read(p []byte) (n int, err error) {
+	n, err = m.rdr.Read(p)
+
+	if n == 0 || err == io.EOF {
+		m.t.Fatal("Read to the end when you shouldn't have")
+	}
+
+	return n, err
+}
+
+type spyBlindAlerter struct {
+	alerts []struct {
+		scheduledAt time.Duration
+		amount      int
+	}
+}
+
+func (s *spyBlindAlerter) ScheduleAlertAt(duration time.Duration, amount int) {
+	s.alerts = append(s.alerts, struct {
+		scheduledAt time.Duration
+		amount      int
+	}{duration, amount})
+}
